82a9dc3ede710ae3f33e1a61ad750d1f5d3813d5
BYUsweet28 <sweet28@byu.edu>
2023-02-06 20:07:26 -0700

    project

---
+++ mission4_sweet28/Controllers/BlahController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission4_sweet28.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission4_sweet28.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            // throw new IndexOutOfRangeException();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            // throw new IndexOutOfRangeException();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(CalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ mission4_sweet28/Models/CalculatorModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission4_sweet28.Models
+{
+    public class CalculatorModel
+    {
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a valid number between 0 and 100")]
+        public int assignments { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a valid number between 0 and 100")]
+        public int groupProject { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a valid number between 0 and 100")]
+        public int quizzes { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a valid number between 0 and 100")]
+        public int midterm { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a valid number between 0 and 100")]
+        public int final { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a valid number between 0 and 100")]
+        public int intex { get; set; }
+    }
+}
